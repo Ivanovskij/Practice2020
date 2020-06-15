6c5be110dd83d6e375efe9831ec0106e0feae1ef
Ivanovskij <webworkk@mail.ru>
2020-06-15 17:08:04 +0300

    add springboot project

---
+++ springboot/src/test/java/org/practice2020/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
