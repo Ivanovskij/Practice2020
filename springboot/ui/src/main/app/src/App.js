5cc29cabfbca1a48f929ca19a23a6350f4657273
Ivanovskij <webworkk@mail.ru>
2020-06-17 13:16:21 +0300

    display user to screen, mock version

--- springboot/ui/src/main/app/src/App.js
+++ springboot/ui/src/main/app/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        isLoading: true,
+        users: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/users/1');
+        const body = await response.json();
+        this.setState({users: body, isLoading: false});
+    }
+
+    render() {
+        const {users, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className="App-intro">
+                        <h2>Users List</h2>
+                            <div key={users.id}>
+                                {users.profile.name}
+                            </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
