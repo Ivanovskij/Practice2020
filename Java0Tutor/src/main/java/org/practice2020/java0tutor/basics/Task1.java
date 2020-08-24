7095ad99a555ebe47de6439d7e1d5a8db93fc56d
Ivanovskij <webworkk@mail.ru>
2020-08-24 21:53:14 +0300

    Develop task1

---
+++ Java0Tutor/src/main/java/org/practice2020/java0tutor/basics/Task1.java
@@ -0,0 +1,12 @@
+package org.practice2020.java0tutor.basics;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println((task1(1, 2.0, 3.2)));
+    }
+
+    public static double task1(double a, double b, double c) {
+        return ((a - 3) * b / 2) - c;
+    }
+}
